930929b6eb3bf2c89dec0ba88da19f80d92a10db
AroLeaf <starleafofa@gmail.com>
2021-12-21 17:55:41 +0100

    changes for core v1.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Module, Bot } from '@ruinguard/core';
 import DB from './db.js';
 
 export default options => {
-  Bot.prototype.db = new DB(options);
+  Bot.prototype.mongo = new DB(options);
 
   return new Module({});
 };
